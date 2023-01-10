9920722ec5e2c250426caec3736a0411a7779bdb
Shrinjay Shresth <104555486+shrinjayshresth1@users.noreply.github.com>
2023-01-10 23:50:40 +0530

    Add files via upload

---
+++ Sentiment Anaysis.py
@@ -0,0 +1,17 @@
+''' IITM BS-MLF Project
+Author:-Shrinjay Shresth/22F000249
+Project Name:-Shrinjay Shresth '''
+
+
+from textblob import TextBlob
+'''Sentiment analyser '''
+y=input("Enter a sentence to analyse:-")
+edu= TextBlob(y)
+x=edu.sentiment.polarity
+# Negative =x<0 and Neutral =0 and Positive =x>0 && x<=1
+if x<0:
+    print("Negative") 
+elif x==0:
+    print("Neutral")
+elif x>0 and x<=1:
+    print("Positive")
